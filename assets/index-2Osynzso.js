7c9a8b49a5c6ef43848b212b00c2628d4c4c871b
Brenda <cyriellecoul@gmail.com>
2026-03-12 15:13:14 -0400

    Updates

--- assets/index-2Osynzso.js
+++ assets/index-2Osynzso.js
@@ -1,4 +1,4 @@
-import{r as _,a as Wn,j as e}from"./index-AW4jqnkM.js";var fo=`svg[fill=none] {
+import{r as _,a as Wn,j as e}from"./index-Dw771icl.js";var fo=`svg[fill=none] {
   fill: none !important;
 }
 
