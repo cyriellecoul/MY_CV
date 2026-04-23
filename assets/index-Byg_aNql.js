01e50df09a4c55954a426df375c52039ee3493d8
Brenda <cyriellecoul@gmail.com>
2026-04-22 21:19:16 -0400

    Updates

--- assets/index-Byg_aNql.js
+++ assets/index-Byg_aNql.js
@@ -1,4 +1,4 @@
-import{r as _,a as Wn,j as e}from"./index-D2Ew9wsj.js";var fo=`svg[fill=none] {
+import{r as _,a as Wn,j as e}from"./index-QnSGH9sF.js";var fo=`svg[fill=none] {
   fill: none !important;
 }
 
