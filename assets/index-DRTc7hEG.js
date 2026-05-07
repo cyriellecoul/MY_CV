965a2808d09f144c6b701b9c5a33716b0654b40a
Brenda <cyriellecoul@gmail.com>
2026-05-07 16:40:13 -0400

    Updates

--- assets/index-DRTc7hEG.js
+++ assets/index-DRTc7hEG.js
@@ -1,4 +1,4 @@
-import{r as _,a as Wn,j as e}from"./index-DIx_zose.js";var fo=`svg[fill=none] {
+import{r as _,a as Wn,j as e}from"./index-KyuCsz-J.js";var fo=`svg[fill=none] {
   fill: none !important;
 }
 
