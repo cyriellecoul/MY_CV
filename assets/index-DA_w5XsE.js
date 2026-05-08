de1b1b30e390d5e5371354f8f6f62b018befebd4
Brenda <cyriellecoul@gmail.com>
2026-05-08 12:51:04 -0400

    Updates

--- assets/index-DA_w5XsE.js
+++ assets/index-DA_w5XsE.js
@@ -1,4 +1,4 @@
-import{r as _,a as Wn,j as e}from"./index-KyuCsz-J.js";var fo=`svg[fill=none] {
+import{r as _,a as Wn,j as e}from"./index-CCsy16Rm.js";var fo=`svg[fill=none] {
   fill: none !important;
 }
 
